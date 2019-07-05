25328cf5ab23dd7e2127ea7de80257bfafca71be
Yifan Song <yifsong@cisco.com>
2019-07-05 08:10:22 -0700

    add string

--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+void readIn() {
+cout<<"read()\n";
+}
+void sort() {
+cout << "sort()\n";
+}
+
+void compact() {
+cout << "compact()\n";
+}
+
+void print() {
+
+cerr <<"print()\n"; 
+}
+
 int main(int argc, char** argv) {
    std::cout<<"Hello World!"<<std::endl;
+string ifile, ofile;
+   readIn();
+   sort();
+   compact();
+   print();
+cout<<"please input your name:\n";
+string x = "this is a good job";
+cout << x <<endl;
+cin>>ifile >> ofile;
    return 0;
 }
