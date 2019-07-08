495162703b1f90bb0ff5cbfe4e88594e31f44aa9
Yifan Song <yifsong@cisco.com>
2019-07-07 18:32:40 -0700

    first step

---
+++ bookstore.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void readIn() {
+cout<<"read()\n";
+}
+void sort() {
+cout << "sort()\n";
+}
+
+void compact() {
+cout << "compact()\n";
+}
+
+void write() {
+
+cout <<"cout()\n";
+}
